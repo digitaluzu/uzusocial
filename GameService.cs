0ee06d86f74fcf99e3b53bf8fb1d2ee8ecc3301c
Lovisa Lionel <liory26@hotmail.com>
2014-03-21 13:33:00 +0100

    Add temporary gamestick support (still not working as I need to set proper leaderboard ID and so)

--- GameService.cs
+++ GameService.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.SocialPlatforms.GameCenter;
 
+#if UZU_GAMESTICK
+using PlayJamUnity;
+#endif
 namespace Uzu
 {
 	/// <summary>
@@ -9,23 +12,30 @@ namespace Uzu
 	/// </summary>
 	public class GameService
 	{
-		#region Authentication 
+		#region Authentication
+
 		public static void Authenticate (System.Action<bool> onAuthentication)
 		{
 #if UNITY_IPHONE && !UNITY_EDITOR
 			if (!IsUserAuthenticated ()) {
 				Social.localUser.Authenticate (onAuthentication);	
 			}
-#elif UNITY_ANDROID && !UNITY_EDITOR
+#elif UNITY_ANDROID && !UNITY_EDITOR && !UZU_GAMESTICK
 			if((!IsUserAuthenticated ())){
 				GooglePlay.instance.Authenticate(onAuthentication);
+			}
+#elif UZU_GAMESTICK
+			{
+				//Do nothing?					
 			}
 #endif
 		}
 
 		public static bool IsUserAuthenticated ()
 		{
-#if UNITY_EDITOR
+#if UZU_GAMESTICK
+			return true;							
+#elif UNITY_EDITOR
 			return false;   
 #elif UNITY_ANDROID 
 			return GooglePlay.instance.IsUserAuthenticated();
@@ -33,6 +43,7 @@ namespace Uzu
 			return Social.localUser.authenticated;
 #endif
 		}
+
 		#endregion
 
 		/// <summary>
@@ -49,18 +60,24 @@ namespace Uzu
 		public static void ShowAchievementUI ()
 		{
 			if (IsUserAuthenticated ()) {
-#if UNITY_ANDROID 
+#if UZU_GAMESTICK
+				//TODO ... I should call a pannel change to show to custome UI but I do it direcly now ("Because the panel is game related")
+				//Maybe I should set a callback?
+#elif UNITY_ANDROID 
 				GooglePlay.instance.ShowAchievementUI();
 #else
 				Social.ShowAchievementsUI ();
 #endif
 			}
 		}
-	
+
 		public static void ShowLeaderboardUI (string leaderboardId, TimeScope timeScope)
 		{
 			if (IsUserAuthenticated ()) {
-#if UNITY_ANDROID 
+#if UZU_GAMESTICK
+				//TODO ... I should call a pannel change to show to custome UI but I do it direcly now ("Because the panel is game related")
+				//Maybe I should set a callback?
+#elif UNITY_ANDROID 
 				GooglePlay.instance.ShowLeaderboardUI (leaderboardId);
 #else
 				GameCenterPlatform.ShowLeaderboardUI (leaderboardId, timeScope);
@@ -71,18 +88,24 @@ namespace Uzu
 		public static void ReportScore (long score, string leaderboardID)
 		{
 			if (IsUserAuthenticated ()) {
-#if UNITY_ANDROID 
+#if UZU_GAMESTICK
+				//TODO the leaderBoardID need to be formated now it won't work
+				PlayJamServices.LeaderBoard_SaveScore ((int)score, 0);
+#elif UNITY_ANDROID 
 				GooglePlay.instance.ReportScore (score, leaderboardID);
 #else
 				Social.ReportScore (score, leaderboardID, result => { /*TODO*/});
 #endif
 			}
 		}
-		
+
 		public static void ReportAchievementProgress (string achievementID, float progress)
 		{
 			if (IsUserAuthenticated ()) {
-#if UNITY_ANDROID 
+#if UZU_GAMESTICK
+				//TODO the leaderBoardID need to be formated now it won't work
+				PlayJamServices.Achievement_SetAchievementComplete (achievementID);
+#elif UNITY_ANDROID  
 				GooglePlay.instance.ReportAchievementProgress (achievementID, progress);
 #else
 				Social.ReportProgress (achievementID, progress, result => {/*TODO*/});
